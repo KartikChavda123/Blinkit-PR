155b507fd5754ac65cafb3819268f1e20e5d24ca
KartikChavda123 <kartikchavda2005@gmail.com>
2025-01-06 13:30:46 +0530

    Blinkit-PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Milk from './Components/Pav/Milk'
 import Nav from './Components/Nav'
-
+import Footer from './Components/Footer'
 export default function App() {
   return (
     <div>
       <Nav/>
       <Milk/>
+      <Footer/>
     </div>
   )
 }
